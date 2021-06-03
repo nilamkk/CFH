1af4c42c31d91fbb77e6838eabf1ab9643edafc9
Nilam Kummar Kalita <nilamkk54438@gmail.com>
2021-06-03 20:10:11 +0530

    two new end points added

--- node_api/routers/profile.js
+++ node_api/routers/profile.js
@@ -1,19 +1,17 @@
+const { performance } = require("perf_hooks")
 const express= require('express')
 const axios=require('axios')
 const moment=require('moment')
-const {getCategory}=require('../utils/functions')
+const {getCategory,getRatingProblemsDist}=require('../utils/functions')
 
 
 const router = new express.Router()
-
-
-
 
 router.get('/yo',(req,res)=>{
     res.send("YOU !!!")
 })
 
-router.get('/contest-rating',async (req,res)=>{
+router.get('/contest-rating',async (req,res)=>{ 
     try{
         const handle="striver_79" // have to receive from authentication
         const data=await axios.get(`https://codeforces.com/api/user.rating?handle=${handle}`) 
@@ -36,13 +34,139 @@ router.get('/contest-rating',async (req,res)=>{
     }
 })
 
+router.get('/problems-rating',async (req,res)=>{
+    // rating - #problems accepted in that rating
+    try{
+        const handle="striver_79" // have to receive from authentication
+        const data=await axios.get(`https://codeforces.com/api/user.status?handle=${handle}`) 
+        const dataToSend= getRatingProblemsDist(data.data.result)
+        res.send(dataToSend)
+    }catch(error){
+        console.log(error)
+    }
+})
 
+router.get('/user-profile',async (req,res)=>{ 
+    try{
+        // const handle="striver_79" // have to receive from authentication
+        const handle="Hawkeye_2000"
+        const data=await axios.get(`https://codeforces.com/api/user.info?handles=${handle}`) 
 
+        const result=data.data.result[0]
 
+        res.send({
+            "firstName":result.firstName,
+            "lastName":result.lastName,
+            "lastVisit":moment(result.lastOnlineTimeSeconds*1000).fromNow(),
+            "country":result.country,
+            "city":result.city,
+            "rating":result.rating,
+            "maxRating":result.maxRating,
+            "photo":"have to send",
+            "handle":result.handle,
+            "category":result.rank,
+            "maxCategory":result.maxRank
+        })
+    }catch(error){
+        console.log(error)
+    }
+})
 
+router.get('/submissions-pi-info',async (req,res)=>{
+    // FAILED, OK, PARTIAL, COMPILATION_ERROR, RUNTIME_ERROR, WRONG_ANSWER, PRESENTATION_ERROR, TIME_LIMIT_EXCEEDED, MEMORY_LIMIT_EXCEEDED, IDLENESS_LIMIT_EXCEEDED, SECURITY_VIOLATED, CRASHED, INPUT_PREPARATION_CRASHED, CHALLENGED, SKIPPED, TESTING, REJECTED. Can be absent.
+    try{
+        const handle="Um_nik" // For Um_nik, it took 78.62482610000112 seconds,60.72122929999977
+        // const handle="striver_79" // For striver_79, it took 18.726451600000264 seconds.
+        // const handle="Hawkeye_2000" // For Hawkeye_2000, it took 6.485499400999397 seconds,3.2269618999995293, 
 
+        ////////////////////-----------------------------------------------------------------------------------------
+        // var t0 = performance.now()
+        ////////////////////-----------------------------------------------------------------------------------------
 
+        const data=await axios.get(`https://codeforces.com/api/user.status?handle=${handle}`) 
+        const result=data.data.result
+    
+        const dataToSend=new Map()
+        let len=result.length,i=0;
+        while(i<len){
+            if(!dataToSend[result[i].verdict])
+                dataToSend[result[i].verdict]=1
+            else
+                dataToSend[result[i].verdict]+=1
+            i++;
+        }
 
+        
+        ////////////////////-----------------------------------------------------------------------------------------
+        // var t1 = performance.now()
+        // console.log("For "+ handle+", it took " + (t1 - t0)/1000 + " seconds.")
+        ////////////////////-----------------------------------------------------------------------------------------
+        res.send(dataToSend)
+    }catch(error){
+        console.log(error)
+    }
+})
 
 
 module.exports= router
+
+
+
+        // {
+        //     "FAILED":0, 
+        //     "OK":0, 
+        //     "PARTIAL":0, 
+        //     "COMPILATION_ERROR":0, 
+        //     "RUNTIME_ERROR":0, 
+        //     "WRONG_ANSWER":0, 
+        //     "PRESENTATION_ERROR":0, 
+        //     "TIME_LIMIT_EXCEEDED":0, 
+        //     "MEMORY_LIMIT_EXCEEDED":0, 
+        //     "IDLENESS_LIMIT_EXCEEDED":0, 
+        //     "SECURITY_VIOLATED":0, 
+        //     "CRASHED":0, 
+        //     "INPUT_PREPARATION_CRASHED":0, 
+        //     "CHALLENGED":0, 
+        //     "SKIPPED":0, 
+        //     "TESTING":0, 
+        //     "REJECTED":0,
+        //     "unknown":0 //Can_be_absent
+        // }
+           // if( item.verdict==="FAILED" ){
+            //     dataToSend["FAILED"]+=1;
+            // }else if(item.verdict==="OK"){
+            //     dataToSend["OK"]+=1;
+            // }else if(item.verdict==="PARTIAL"){
+            //     dataToSend["PARTIAL"]+=1;
+            // }else if(item.verdict==="COMPILATION_ERROR"){
+            //     dataToSend["COMPILATION_ERROR"]+=1;
+            // }else if(item.verdict==="RUNTIME_ERROR"){
+            //     dataToSend["RUNTIME_ERROR"]+=1;
+            // }else if(item.verdict==="WRONG_ANSWER"){
+            //     dataToSend["WRONG_ANSWER"]+=1;
+            // }else if(item.verdict==="PRESENTATION_ERROR"){
+            //     dataToSend["PRESENTATION_ERROR"]+=1;
+            // }else if(item.verdict==="TIME_LIMIT_EXCEEDED"){
+            //     dataToSend["TIME_LIMIT_EXCEEDED"]+=1;
+            // }else if(item.verdict==="MEMORY_LIMIT_EXCEEDED"){
+            //     dataToSend["MEMORY_LIMIT_EXCEEDED"]+=1;
+            // }else if(item.verdict==="IDLENESS_LIMIT_EXCEEDED"){
+            //     dataToSend["IDLENESS_LIMIT_EXCEEDED"]+=1;
+            // }else if(item.verdict==="SECURITY_VIOLATED"){
+            //     dataToSend["SECURITY_VIOLATED"]+=1;
+            // }else if(item.verdict==="CRASHED"){
+            //     dataToSend["CRASHED"]+=1;
+            // }else if(item.verdict==="INPUT_PREPARATION_CRASHED"){
+            //     dataToSend["INPUT_PREPARATION_CRASHED"]+=1;
+            // }else if(item.verdict==="CHALLENGED"){
+            //     dataToSend["CHALLENGED"]+=1;
+            // }else if(item.verdict==="SKIPPED"){
+            //     dataToSend["SKIPPED"]+=1;
+            // }else if(item.verdict==="TESTING"){
+            //     dataToSend["TESTING"]+=1;
+            // }else if(item.verdict==="REJECTED"){
+            //     dataToSend["REJECTED"]+=1;
+            // }else if(item.verdict==="unknown"){
+            //     dataToSend["unknown"]+=1
+            // }
+    
--- node_api/utils/functions.js
+++ node_api/utils/functions.js
@@ -20,4 +20,65 @@ const getCategory=(rating)=>{
     return "legendary grandmaster"
 }
 
-module.exports={getCategory}
+const getProblemId=(problem)=>{
+    // contestId_index_nameFirstLetters_typeFirstLetter_points_rating   
+    let problemId=""
+    if(problem.contestId){
+        problemId+= problem.contestId+"-"
+    }
+    problemId+= problem.index+"-"
+    problem.name.split(" ").forEach((word)=>{
+        problemId+=word[0]
+    })
+    problemId+=problem.type[0]
+    if(problem.points){
+        problemId+="-"+problem.points
+    }
+    if(problem.rating){
+        problemId+="-"+problem.rating
+    }
+    return problemId
+}
+const getRatingProblemsDist=(result)=>{
+    const verdictOk=new Set()
+        const dataToSend={
+            "0-499":0,   //"0-499"
+            "500-999":0, //"500-999":0,
+            "1000-1499":0, //"1000-1499":0,
+            "1500-1999":0, //"1500-1999":0,
+            "2000-2499":0,
+            "2500-2999":0,
+            "3000-3499":0,
+            "3500-inf":0
+        }
+        result.forEach((item)=>{
+            if(item.verdict==="OK"){
+                const problemId=getProblemId(item.problem)
+                if( verdictOk.has(problemId)===false ){
+                    if( item.problem.rating<500){
+                        dataToSend["0-499"]+=1
+                    }else if( 500<=item.problem.rating && item.problem.rating<=999){
+                        dataToSend["500-999"]+=1
+                    }else if(1000<=item.problem.rating && item.problem.rating<=1499){
+                        dataToSend["1000-1499"]+=1
+                    }else if(1500<=item.problem.rating && item.problem.rating<=1999){
+                        dataToSend["1500-1999"]+=1
+                    }else if(2000<=item.problem.rating && item.problem.rating<=2499){
+                        dataToSend["2000-2499"]+=1
+                    }else if(2500<=item.problem.rating && item.problem.rating<=3000){
+                        dataToSend["2500-3000"]+=1
+                    }else if(3000<=item.problem.rating && item.problem.rating<=3499){
+                        dataToSend["3000-3499"]+=1
+                    }else{
+                        dataToSend["3500-inf"]+=1
+                    }
+                    verdictOk.add( problemId ) 
+                }
+            }
+        })
+        return dataToSend
+}
+
+
+
+module.exports={getCategory,getRatingProblemsDist}
